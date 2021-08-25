c01d9231aa08ac48fde77b7277af1793b6e376eb
ikechukwu-peter <pitiyke@gmail.com>
2021-08-24 17:59:21 -0700

    feat: mobile navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-let btn = document.querySelector('.menu-btn')
-let menuBar = document.querySelector('.menu-items')
-
-btn.addEventListener('click', () => {
-    menuBar.classList.toggle('open')
-   })
+let menuBar = document.querySelector('.menu')
+let menuIcons = document.querySelector('.icons-menu')
+const hamburger = document.querySelector('.toggle-button');
+hamburger.addEventListener('click', function() {
+    this.classList.toggle('is-active');
+    menuBar.classList.toggle('is-active');
+    menuIcons.classList.toggle('is-active');
+})
