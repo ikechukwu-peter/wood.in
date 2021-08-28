5909c756c6c06e020395bf90328322dc17e7777f
ikechukwu-peter <pitiyke@gmail.com>
2021-08-28 20:46:39 +0100

    feat: added functonality to links

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 let menuBar = document.querySelector('.menu')
 let menuIcons = document.querySelector('.icons-menu')
+let icons = document.querySelectorAll('.modal-pop')
 const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelectorAll('.links')
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 
 //handles toggling
 hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
     menuBar.classList.toggle('is-active');
     menuIcons.classList.toggle('is-active');
+})
+
+//Toggle active state for links and remove from all the elements
+navLinks.forEach(elements => {
+    elements.addEventListener('click', (e) => {
+        navLinks.forEach(navLink => {
+            navLink.classList.remove('is-active')
+            e.target.classList.add('is-active')
+            menuBar.classList.remove('is-active')
+            hamburger.classList.remove('is-active');
+            menuIcons.classList.remove('is-active');
+        })
+
+    })
 })
 
 
@@ -56,6 +78,29 @@ let text = () => {
     }
 }
 
+
+// MODAL
+// When the user clicks on the button, open the modal
+icons.forEach(icon => {
+    icon.addEventListener('click', (e) => {
+        menuBar.classList.remove('is-active')
+        hamburger.classList.remove('is-active');
+        menuIcons.classList.remove('is-active');
+        modal.style.display = "block"; 
+    })
+})
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 window.onload = () => {
     text()
     AOS.init()
